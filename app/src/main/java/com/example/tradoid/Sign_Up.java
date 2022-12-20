bb107d65c42eeee4235c319a2527f6974ae763dd
Inon-Sinn <inon.sinn@gmail.com>
2022-12-20 20:04:13 +0200

    Foundation: Sign Up UI

--- app/src/main/java/com/example/tradoid/Sign_Up.java
+++ app/src/main/java/com/example/tradoid/Sign_Up.java
@@ -1,12 +1,23 @@
 package com.example.tradoid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Sign_Up extends AppCompatActivity {
 
@@ -22,6 +33,28 @@ public class Sign_Up extends AppCompatActivity {
         // Implementing the Back arrow in the Toolbar
         ImageView back_arrow = findViewById(R.id.sign_up_To_login);
         back_arrow.setOnClickListener(v -> sendToActivity(login.class));
+
+        // Connecting to the Text View "sign in"
+        TextView tv_sign_in = findViewById(R.id.tv_sign_in_sign_up);
+
+        // Making the Text View Clickable -> sends to Sign In
+        String text = "sign in";
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                sendToActivity(Sign_In.class);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+            }
+        };
+        ss.setSpan(clickableSpan,0,text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv_sign_in.setText(ss);
+        tv_sign_in.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     // Sends to other screens
