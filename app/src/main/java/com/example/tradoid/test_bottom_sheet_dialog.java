8471d8a387a3b3bacaa4679d2a7e95083f1ef44c
Inon-Sinn <inon.sinn@gmail.com>
2023-01-03 15:15:59 +0200

    Last additions and fixes: Email Validation Pin

--- app/src/main/java/com/example/tradoid/test_bottom_sheet_dialog.java
+++ app/src/main/java/com/example/tradoid/test_bottom_sheet_dialog.java
@@ -2,26 +2,27 @@ package com.example.tradoid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-
-
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import android.widget.Toast;
+import com.alimuzaffar.lib.pin.PinEntryEditText;
 
 public class test_bottom_sheet_dialog extends AppCompatActivity {
-
-    private BottomSheetBehavior sheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_bottom_sheet_dialog);
 
-        EditText num_1 = findViewById(R.id.validation_num1);
-
-
-
-
-
+        final PinEntryEditText pinEntry = findViewById(R.id.validation_pin);
+        if (pinEntry != null) {
+            pinEntry.setOnPinEnteredListener(str -> {
+                if (str.toString().equals("123456")) {
+                    Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "FAIL", Toast.LENGTH_SHORT).show();
+                    pinEntry.setText(null);
+                }
+            });
+        }
 
     }
 
